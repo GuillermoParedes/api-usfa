7aa48140484893649c2b8fa8a6a889f34936ba17
gd <gdavid.ptorrez@gmail.com>
2019-09-13 03:07:07 -0400

    add service hashtags

--- src/main.controller.ts
+++ src/main.controller.ts
@@ -1,11 +1,13 @@
 import { Application } from 'express';
 import { TwitterService } from './services/twitter.service';
+import { HashTagsService } from './services/hashtags.service';
 
 export class Controller {
   private twitterService: TwitterService;
-
+  private hashTagsService: HashTagsService;
   constructor(private app: Application) {
     this.twitterService = new TwitterService();
+    this.hashTagsService = new HashTagsService();
     this.routes();
   }
 
@@ -13,5 +15,7 @@ export class Controller {
     this.app.route('/').get(this.twitterService.welcomeMessage);
 
     this.app.route('/users/profile').post(this.twitterService.postProfile);
+
+    this.app.route('/search/hashtags').post(this.hashTagsService.findHashTag);
   }
 }
---
+++ src/services/hashtags.service.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import { WELCOME_MESSAGE, API_SEARCH_HASH_TAG } from '../constants/usfa.constants';
+
+import { ClientTwitter } from '../config/ClientTwitter';
+
+export class HashTagsService {
+  public welcomeMessage(req: Request, res: Response) {
+    return res.status(200).send(WELCOME_MESSAGE);
+  }
+
+  public async findHashTag(req: Request, res: Response) {
+    const { values } = req.body;
+    if (typeof values === 'object') {
+      const usersRequest: any = [];
+
+      values.forEach((user: string) => {
+        const params = { q: user };
+        usersRequest.push(ClientTwitter.get(API_SEARCH_HASH_TAG, params));
+      });
+
+      await Promise.all(usersRequest).then((response: any) => {
+        // const users = response.map(function(value: any) {
+        //   return new Profile(value);
+        // });
+        return res.status(200).json({ status: true, data: response });
+      });
+    } else if (typeof values === 'string') {
+      const params = { q: values };
+
+      ClientTwitter.get(API_SEARCH_HASH_TAG, params, function(error: any, tweets: any, response: any) {
+        if (!error) {
+          return res.status(200).json({ status: true, data: [tweets] });
+        }
+        return res.status(404).send(error);
+      });
+    } else {
+      return res.status(404).send(values);
+    }
+  }
+}
