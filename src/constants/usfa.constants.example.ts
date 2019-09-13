91b2a2c5075a10096547f8c136e5240d0630b7bc
gd <gdavid.ptorrez@gmail.com>
2019-09-13 02:55:05 -0400

    add constants.example

---
+++ src/constants/usfa.constants.example.ts
@@ -0,0 +1,7 @@
+export const PORT = 9001;
+export const WELCOME_MESSAGE = 'Welcome to TwitterAPI REST by GD_DEV ^_^!';
+
+export const CONSUMER_KEY = 'YOUR CONSUMER KEY';
+export const CONSUMER_SECRET = 'YOUR CONSUMER SECRET';
+export const ACCESS_TOKEN_KEY = 'YOUR ACCESS TOKEN KEY';
+export const ACCESS_TOKEN_SECRET = 'YOUR ACCESS TOKEN SECRET';
