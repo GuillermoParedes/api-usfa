1cc896279da005c1d85cb296c9427bbae5b7aaf8
gd <gdavid.ptorrez@gmail.com>
2019-09-13 03:27:47 -0400

    add docs API

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import app from './app';
 import { PORT } from './constants/usfa.constants';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../swagger.json';
 
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
